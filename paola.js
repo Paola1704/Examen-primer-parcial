1d10a608359d5c66b022b92a029206dd12d541cb
Paola1704 <nahhernandezpaola@gmail.com>
2025-04-07 15:43:39 -0600

    Add files via upload

---
+++ paola.js
@@ -0,0 +1,7 @@
+saludar ()
+let nombre= ("Ingrese su nombre");
+alert ("Hola" + nombre + "suerte en tu examen");
+calcularCuadrado ()
+let num= parseInt(prompt("Ingresa el numero"));
+let numCuadrado= num^2
+alert ("El cuadrado es" + numCuadrado + );
