b4c8220c5e1be3df917e3adf50be4bdacc4e6eaa
Paola1704 <nahhernandezpaola@gmail.com>
2025-04-07 17:43:24 -0600

    Update paola.js

--- paola.js
+++ paola.js
@@ -1,11 +1,9 @@
-function saludar ()
-{
-  let nombre= ("Ingrese su nombre");
-  alert ("Hola" + nombre + "suerte en tu examen");
+function saludar () {
+  let nombre = ("Ingrese su nombre");
+  alert("Hola" + nombre + "suerte en tu examen");
 }
-function calcularCuadrado ()
-{
-  let num= parseInt(prompt("Ingresa el numero"));
-  let numCuadrado= num^2
-  alert ("El cuadrado es" + numCuadrado + );
+function calcularCuadrado () {
+  let num = parseInt(prompt("Ingresa el numero"));
+  let numCuadrado = num^2
+  alert("El cuadrado es" + numCuadrado + );
 }
